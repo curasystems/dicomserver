5e91d5260dfa2fe7b36fb3bcbfbf5d7d880bf870
matthiasg <m.goetzke@curasystems.de>
2011-05-04 11:26:59 +0200

    added cmove test program and empty testclient for later

---
+++ cmove/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Dicom.Network;
+using Dicom.Network.Server;
+
+namespace cmove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seriesUidArg = args.FirstOrDefault(a => a.StartsWith("/seriesuid="));
+
+            StartStorageServer(10108);
+
+            //if( !String.IsNullOrEmpty(seriesUidArg) )
+            {
+                var seriesUid = "1.3.12.2.1107.5.2.32.35322.2010082109092032370711447.0.0.0";
+
+                if( !String.IsNullOrEmpty(seriesUidArg) )
+                    seriesUid = seriesUidArg.Split('=')[1];
+
+                TransferSeries(seriesUid);
+            }
+
+            StopStorageServer();
+        }
+
+        private static void StopStorageServer()
+        {
+            _storageServer.Stop();
+        }
+
+        private static void StartStorageServer(int port)
+        {
+            _storageServer = new Dicom.Network.Server.DcmServer<CStoreService>();
+            _storageServer.AddPort(port, DcmSocketType.TCP);
+            _storageServer.OnDicomClientCreated = (s, c, t) =>
+            {
+                c.UseFileBuffer = false;
+                //c.OnCStoreRequestProgress +=
+                //    (client, pcid, command, dataset, progress) =>
+                //        {
+                //         //   Console.WriteLine("{0} {1}%",progress.BytesTransfered,
+                //         //                     (double)progress.BytesTransfered/progress.EstimatedBytesTotal);
+
+                //        };
+            };
+            _storageServer.Start();
+        }
+
+        private static void TransferSeries(string seriesUid)
+        {
+            var moveClient = new Dicom.Network.Client.CMoveClient();
+            moveClient.DestinationAE = callingAE;
+            moveClient.CallingAE = callingAE;
+            moveClient.CalledAE = calledAE;
+            moveClient.AddQuery(DcmQueryRetrieveLevel.Series, seriesUid);
+            
+            moveClient.OnCMoveResponse += (query, dataset, status, remain, complete, warning, failure) =>
+                                              {
+                                                  if( status == DcmStatus.Pending )
+                                                        Console.WriteLine("{0}/{1} (warn:{2}, fail:{3})", complete, remain, warning,failure);
+                                                  else if( status == DcmStatus.Success )
+                                                  {
+                                                      Console.WriteLine("{0}/{1} (warn:{2}, fail:{3})", complete, remain, warning,failure);
+                                                      Console.WriteLine("Completed.");
+                                                  }
+                                                  else
+                                                      Console.WriteLine("Status:{0}", status);
+                                              };
+
+            moveClient.Connect(target.Address.ToString(), target.Port, DcmSocketType.TCP);
+            moveClient.Wait();
+        }
+
+        private static IPEndPoint target = new IPEndPoint( IPAddress.Parse("10.32.34.37"), 10105);
+        private static string callingAE = "CURACLIENT";
+        private static string calledAE = "CURAPACS";
+        private static DcmServer<CStoreService> _storageServer;
+    }
+}
---
+++ dicomserver/testclient/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//using Dicom;
+//using Dicom.Network;
+
+//using Dicom.Data;
+
+namespace testclient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new Dicom.Network.Client.CFindStudyClient
+                             {
+                                 CallingAE = "CURATEST", CalledAE = "CURAPACS"
+                             };
+
+            var q = new Dicom.Network.Client.CFindStudyQuery();
+                        //{
+                        //    StudyDate = new DcmDateRange(DateTime.Now.Subtract(TimeSpan.FromDays(1)))
+                        //};
+
+            client.AddQuery( q );
+            client.Connect("127.0.0.1", 10105, Dicom.Network.DcmSocketType.TCP);
+            
+            client.OnCFindResponse = (oq, r) =>
+                                         {
+                                             listBox1.Items.Add(r);
+                                         };
+            client.OnCFindComplete = (oq, r) =>
+                                         {
+                                             
+                                         };
+
+
+        }
+    }
+}
