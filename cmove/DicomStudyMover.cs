6309e661cb9cfa967925aba6e7ef0a43367cb8c6
Matthias Goetzke <m.goetzke@curasystems.de>
2011-07-28 18:14:45 +0200

    added dicom study mover

---
+++ cmove/DicomStudyMover.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using Dicom.Network;
+using Dicom.Network.Client;
+
+namespace cmove
+{
+    public class DicomStudyMover
+    {
+        const string _callingAE = "CURACLIENT";
+        string _serverAE = "CURAPACS";
+        IPEndPoint _serverAddress;
+
+        public DicomStudyMover(string serverAddress)
+        {
+            ParseServerAddress(serverAddress);
+        }
+
+        void ParseServerAddress(string address)
+        {
+            string[] parts = address.Split(new[] {'@'}, 2);
+
+            if (parts.Length != 2)
+            {
+                Console.WriteLine("Server Address must be in format <serverae>@<serverip>:<port>");
+
+                throw new InvalidOperationException();
+            }
+
+            _serverAE = parts[0];
+            _serverAddress = ParseIPAddress(parts[1]);
+        }
+
+        static IPEndPoint ParseIPAddress(string address)
+        {
+            string[] parts = address.Split(new[] {':'}, 2);
+
+            if (parts.Length == 1)
+            {
+                return new IPEndPoint(IPAddress.Parse(parts[0]), 104);
+            }
+            else
+            {
+                return new IPEndPoint(IPAddress.Parse(parts[0]), UInt16.Parse(parts[1]));
+            }
+        }
+
+        public void TransferStudy(string studyUid, string targetAE)
+        {
+            var moveClient = new CMoveClient();
+            moveClient.DestinationAE = targetAE;
+            moveClient.CallingAE = _callingAE;
+            moveClient.CalledAE = _serverAE;
+            moveClient.AddQuery(DcmQueryRetrieveLevel.Study, studyUid);
+
+            moveClient.OnCMoveResponse += (query, dataset, status, remain, complete, warning, failure) =>
+                {
+                    Console.Clear();
+
+                    if (status == DcmStatus.Pending)
+                        Console.WriteLine("{0}/{1} (warn:{2}, fail:{3})", complete, remain, warning, failure);
+                    else if (status == DcmStatus.Success)
+                    {
+                        Console.WriteLine("{0}/{1} (warn:{2}, fail:{3})", complete, remain, warning, failure);
+                        Console.WriteLine("Completed.");
+                    }
+                    else
+                        Console.WriteLine("Status:{0}", status);
+                };
+
+            Console.WriteLine("Initiating C-MOVE of {0} from {1} to {2}...", studyUid, _serverAddress, targetAE);
+
+            moveClient.Connect(_serverAddress.Address.ToString(), _serverAddress.Port, DcmSocketType.TCP);
+            moveClient.Wait();
+        }
+    }
+}
--- cmove/Program.cs
+++ cmove/Program.cs
@@ -12,23 +12,16 @@ namespace cmove
     {
         static void Main(string[] args)
         {
-            var seriesUidArg = args.FirstOrDefault(a => a.StartsWith("/seriesuid="));
-
-            StartStorageServer(10108);
-
-            //if( !String.IsNullOrEmpty(seriesUidArg) )
+            if (args.Length < 3)
             {
-                var seriesUid = "1.3.12.2.1107.5.2.32.35322.2010082109092032370711447.0.0.0";
-
-                if( !String.IsNullOrEmpty(seriesUidArg) )
-                    seriesUid = seriesUidArg.Split('=')[1];
-
-                TransferSeries(seriesUid);
+                Console.WriteLine("cmove <serverae>@<serverip>:<port> <studyuid> <targetae>");
+                return;
             }
 
-            StopStorageServer();
+            var mover = new DicomStudyMover(args[0]);
+            mover.TransferStudy( args[1], args[2] );
         }
-
+        
         private static void StopStorageServer()
         {
             _storageServer.Stop();
@@ -52,34 +45,7 @@ namespace cmove
             _storageServer.Start();
         }
 
-        private static void TransferSeries(string seriesUid)
-        {
-            var moveClient = new Dicom.Network.Client.CMoveClient();
-            moveClient.DestinationAE = callingAE;
-            moveClient.CallingAE = callingAE;
-            moveClient.CalledAE = calledAE;
-            moveClient.AddQuery(DcmQueryRetrieveLevel.Series, seriesUid);
-            
-            moveClient.OnCMoveResponse += (query, dataset, status, remain, complete, warning, failure) =>
-                                              {
-                                                  if( status == DcmStatus.Pending )
-                                                        Console.WriteLine("{0}/{1} (warn:{2}, fail:{3})", complete, remain, warning,failure);
-                                                  else if( status == DcmStatus.Success )
-                                                  {
-                                                      Console.WriteLine("{0}/{1} (warn:{2}, fail:{3})", complete, remain, warning,failure);
-                                                      Console.WriteLine("Completed.");
-                                                  }
-                                                  else
-                                                      Console.WriteLine("Status:{0}", status);
-                                              };
-
-            moveClient.Connect(target.Address.ToString(), target.Port, DcmSocketType.TCP);
-            moveClient.Wait();
-        }
-
         private static IPEndPoint target = new IPEndPoint( IPAddress.Parse("10.32.34.37"), 10105);
-        private static string callingAE = "CURACLIENT";
-        private static string calledAE = "CURAPACS";
         private static DcmServer<CStoreService> _storageServer;
     }
 }
